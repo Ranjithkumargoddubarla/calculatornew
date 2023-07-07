fa313c954ba96cd2790de6e65101c106a3d79e61
Ranjithkumargoddubarla <138871868+Ranjithkumargoddubarla@users.noreply.github.com>
2023-07-07 16:09:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+var clickSound1=new Audio()
+var clickSound2=new Audio()
+var clickSound3=new Audio()
+var clickSound4=new Audio()
+var clickSound5=new Audio()
+var clickSound6=new Audio()
+var clickSound7=new Audio()
+var clickSound8=new Audio()
+var clickSound9=new Audio()
+var clickSound10=new Audio()
+var clickSound11=new Audio()
+var clickSound12=new Audio()
+
+clickSound1.src="./click.wav"
+clickSound2.src="./click1.mp3"
+clickSound3.src="./click2.mp3"
+clickSound4.src="./click3.mp3"
+clickSound5.src="./click4.mp3"
+clickSound6.src="./click5.mp3"
+clickSound7.src="./click6.mp3"
+clickSound8.src="./click7.mp3"
+clickSound9.src="./click8.mp3"
+clickSound10.src="./click9.mp3"
+clickSound11.src="./click10.mp3"
+clickSound12.src="./click11.mp3"
+
+function clickAudio()
+{
+    clickSound1.play()
+}
+function clickAudio1()
+{
+    clickSound2.play()
+}
+function clickAudio2()
+{
+    clickSound3.play()
+}
+function clickAudio3()
+{
+    clickSound4.play()
+}
+function clickAudio4()
+{
+    clickSound5.play()
+}
+function clickAudio5()
+{
+    clickSound6.play()
+}
+function clickAudio6()
+{
+    clickSound7.play()
+}
+function clickAudio7()
+{
+    clickSound8.play()
+}
+function clickAudio8()
+{
+    clickSound9.play()
+}
+function clickAudio9()
+{
+    clickSound10.play()
+}
+function clickAudio10()
+{
+    clickSound11.play()
+}
+function clickAudio11()
+{
+    clickSound12.play()
+}
